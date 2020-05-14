673d980b5e43986cec1cc70ec0509ae233cb0ec8
Bhaskar141997 <65129278+Bhaskar141997@users.noreply.github.com>
2020-05-14 12:03:30 +0530

    13-05-2020 [Q1-Q5] assignment 3

---
+++ 13 may 2020/question2.cpp
@@ -0,0 +1,46 @@
+// C program to multiply two square matrices. 
+#include <stdio.h> 
+#define N 4 
+  
+// This function multiplies mat1[][] and mat2[][], 
+// and stores the result in res[][] 
+void multiply(int mat1[][N], int mat2[][N], int res[][N]) 
+{ 
+    int i, j, k; 
+    for (i = 0; i < N; i++) 
+    { 
+        for (j = 0; j < N; j++) 
+        { 
+            res[i][j] = 0; 
+            for (k = 0; k < N; k++) 
+                res[i][j] += mat1[i][k]*mat2[k][j]; 
+        } 
+    } 
+} 
+  
+int main() 
+{ 
+    int mat1[N][N] = { {1, 1, 1, 1}, 
+                    {2, 2, 2, 2}, 
+                    {3, 3, 3, 3}, 
+                    {4, 4, 4, 4}}; 
+  
+    int mat2[N][N] = { {1, 1, 1, 1}, 
+                    {2, 2, 2, 2}, 
+                    {3, 3, 3, 3}, 
+                    {4, 4, 4, 4}}; 
+  
+    int res[N][N]; // To store result 
+    int i, j; 
+    multiply(mat1, mat2, res); 
+  
+    printf("Result matrix is \n"); 
+    for (i = 0; i < N; i++) 
+    { 
+        for (j = 0; j < N; j++) 
+           printf("%d ", res[i][j]); 
+        printf("\n"); 
+    } 
+  
+    return 0; 
+} 
---
+++ 13 may 2020/question4.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int n, reversedN = 0, remainder, originalN;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    originalN = n;
+
+    // reversed integer is stored in reversedN
+    while (n != 0) {
+        remainder = n % 10;
+        reversedN = reversedN * 10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if orignalN and reversedN are equal
+    if (originalN == reversedN)
+        printf("%d is a palindrome.", originalN);
+    else
+        printf("%d is not a palindrome.", originalN);
+
+    return 0;
+}
+
